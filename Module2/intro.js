0b48310d445e658856c2e058418d00db0109f3a6
Akash-Kumar <akashbnsdav@gmail.com>
2022-04-17 00:51:11 +0530

    intro to js

---
+++ Module2/intro.js
@@ -0,0 +1,27 @@
+// this is for printing
+console.log("Swagat nhi karoge hamara");
+
+//variables in javaScript
+
+let num = 10; //number
+console.log(num);
+
+let char = 'a';
+console.log(char);
+
+let Str = "I'm a Pepcoder";
+console.log(Str);
+
+let bool = true;
+console.log(bool);
+
+for(let i=1; i<=5; i++){
+    console.log(i);
+}
+
+let count = 10;
+while(count>0){
+    console.log(count);
+    count--;
+}
+
