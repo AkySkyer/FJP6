dba7704a522a78b8a83a3a9ba69030e0308ba99a
Akash-Kumar <akashbnsdav@gmail.com>
2022-04-17 18:24:08 +0530

    string in Js and methods

---
+++ Module2/string.js
@@ -0,0 +1,40 @@
+let str = "I'm am string"
+
+// length of string
+let len = str.length;
+console.log(len)
+
+// sliced method
+
+let slicedArr = str.slice(2,10);
+console.log(slicedArr);
+
+//replace method
+
+let replaceStr = str.replace("am", "was");
+console.log(replaceStr)
+
+//upper case
+
+let upperCase = str.toUpperCase();
+console.log(upperCase);
+
+// lower case
+
+let lowercase = str.toLowerCase();
+console.log(lowercase);
+
+//concanate
+
+let first = "Aky";
+let sec = " Skyer"
+let fullName = first.concat(sec);
+console.log(fullName)
+
+//or
+
+console.log(first + sec)
+
+// to use any method just write variable and put (.)
+
+// eg ------>>>>>>>> str.
