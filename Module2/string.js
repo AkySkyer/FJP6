037a62715fcf8c253731ea456969f134132c0693
Akash-Kumar <akashbnsdav@gmail.com>
2022-04-18 16:52:31 +0530

    data from freecodecamps

--- Module2/string.js
+++ Module2/string.js
@@ -6,7 +6,7 @@ console.log(len)
 
 // sliced method
 
-let slicedArr = str.slice(2,10);
+let slicedArr = str.slice(2, 10);
 console.log(slicedArr);
 
 //replace method
@@ -38,3 +38,19 @@ console.log(first + sec)
 // to use any method just write variable and put (.)
 
 // eg ------>>>>>>>> str.
+
+// var and let---->>> var is so weak u csn declare 2 variable to it and it will change to the latest.. you might accidentally overwrite a variable that you did not intend to
+var catName = "nini";
+var catName = "meow";
+console.log(catName);
+
+//but let don't let u u do it.. ES6
+let catSound = "grrrr";
+//let catSound = "meooooowwww";
+console.log(catSound);
+
+// then there comes "const" keyword which only let you  read-only.
+
+const myName = "Skyer";
+const myStr = "My name is " + myName + " and I am well";
+console.log(myStr)
